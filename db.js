8f5f009d4303176fef2bda36da611d3f80e439ad
João Bagatoli <joaovitorbagatoli07@gmail.com>
2025-03-16 18:30:06 -0300

    fix and improvements

--- db.js
+++ db.js
@@ -1,23 +1,30 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 async function connectDbAsync() {
     if (hasDbConnection()) return global.connection;
-    const connection = await createDbConnectionAsync();
-    global.connection = connection;
-    return connection;
+
+    try {
+        const connection = await createDbConnectionAsync();
+        global.connection = connection;
+        return connection;
+    } catch(error) {
+        console.error('Error connecting to database', error);
+        throw error;
+    }
 }
 
 function hasDbConnection() {
-    return global.connection && global.connection !== 'disconnected';
+    return global.connection && global.connection.state !== 'disconnected';
 }
 
 async function createDbConnectionAsync() {
     return await mysql.createConnection({
-        host: 'host',
-        port: 3306,
-        user: 'root',
-        password: '',
-        database: 'codelab'
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     });
 }
 
