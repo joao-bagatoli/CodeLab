ddf0e3d5107deb6567dc1c0d6be4c2aca3817297
João Bagatoli <joaovitorbagatoli07@gmail.com>
2025-06-01 11:37:40 -0300

    FEAT: melhoria visual feedback erro usuários admin

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,28 +2,32 @@ const userService = require('../services/user.service');
 
 class UserController {
     static async getUsers(req, res) {
+        const userLogged = req.session.user;
+
         try {
-            const user = req.session.user;
             const users = await userService.getUsersAsync();
-            return res.render('admin/users', { user, users });
+            return res.render('admin/users', { user: userLogged, users, error: null });
         } catch {
-            return res.render('admin/users', { user, users: null });
+            return res.render('admin/users', { user: userLogged, users: [], error: 'Erro ao carregar usuários' });
         }
     }
 
     static async addUser(req, res) {
+        const userLogged = req.session.user;
+
         try {
             const { name, email, isAdmin } = req.body;
             const user = { name, email, isAdmin: isAdmin === 'on' ? 1 : 0 };
             await userService.addUserAsync(user);
             res.redirect('/admin/users');
         } catch {
-            const userLogged = req.session.user;
-            return res.render('admin/users', { user: userLogged, users: null });
+            return res.render('admin/users', { user: userLogged, users: [], error: 'Erro ao adicionar usuário' });
         }
     }
 
     static async updateUser(req, res) {
+        const userLogged = req.session.user;
+
         try {
             const { isAdmin } = req.body;
             const userId = req.params.id;
@@ -31,19 +35,19 @@ class UserController {
             await userService.updateUserAsync(userId, adminFlag);
             res.redirect('/admin/users');
         } catch {
-            const userLogged = req.session.user;
-            return res.render('admin/users', { user: userLogged, users: null });
+            return res.render('admin/users', { user: userLogged, users: [], error: 'Erro ao atualizar usuário' });
         }
     }
 
     static async deleteUser(req, res) {
+        const userLogged = req.session.user;
+
         try {
             const userId = req.params.id;
             await userService.deleteUserAsync(userId);
             res.redirect('/admin/users');
         } catch {
-            const userLogged = req.session.user;
-            return res.render('admin/users', { user: userLogged, users: null });
+            return res.render('admin/users', { user: userLogged, users: [], error: 'Erro ao deletar usuário' });
         }
     }
 }
